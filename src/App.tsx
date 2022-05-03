0e191198a98132a2edb1aa54ea86c5200ae30e2d
Laytep <nazar.mishchenia@gmail.com>
2022-05-03 18:44:12 +0300

    UserList

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Card, { CardVariant } from './components/Card';
+import UserList from './components/UserList';
+import { IUser } from './types/types';
 
 const App = () => {
+  const users: IUser[] = [
+    {id: 1, name: 'Laytep', email: 'XYZ@gmail.com', address: {city: 'Warsaw', street: 'Suwalska', zipcode: '123'}},
+    {id: 2, name: 'Jessy', email: 'jessy@gmail.com', address: {city: 'Warsaw', street: 'Elska', zipcode: '322'}}
+    
+  ]
   return (
     <div>
       <Card onClick={(num)=> console.log('click', num)} variant={CardVariant.outlined} width='200px' height='200px'>
         <button>Button</button>
         <div>XYZ</div>
       </Card>
+        <UserList users={users}></UserList>
     </div>
   );
 };
