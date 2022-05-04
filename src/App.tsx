c7e59653ef150cf00c0709a6bf699a3fa27ac581
Laytep <nazar.mishchenia@gmail.com>
2022-05-04 12:49:00 +0300

    UseRefTipisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './components/Card';
+import EventsExample from './components/EventsExample';
+import List from './components/List';
+import TodoItem from './components/TodoItem';
+import UserItem from './components/UserItem';
 import UserList from './components/UserList';
-import { IUser } from './types/types';
+import { ITodo, IUser } from './types/types';
 
 const App = () => {
     const [users, setUsers] = useState<IUser[]>([]);
+    const [todos, setTodos] = useState<ITodo[]>([]);
 
     useEffect(() => {
         fetchUsers()
+        fetchTodos()
     }, [])
 
     async function fetchUsers() {
@@ -19,14 +25,32 @@ const App = () => {
             alert(e)
         }
     }
+
+    async function fetchTodos() {
+        try {
+            const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
+            setTodos(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
  
 return (
     <div>
-      <Card onClick={(num)=> console.log('click', num)} variant={CardVariant.outlined} width='200px' height='200px'>
-        <button>Button</button>
-        <div>XYZ</div>
-      </Card>
-        <UserList users={users}></UserList>
+        <EventsExample/>
+        <Card onClick={(num)=> console.log('click', num)} variant={CardVariant.outlined} width='200px' height='200px'>
+            <button>Button</button>
+            <div>XYZ</div>
+        </Card>
+        
+        <List
+            items={users}
+            renderItem={(user: IUser) => <UserItem user={user} key={user.id}/>}
+        />
+        <List
+            items={todos}
+            renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id}/>}
+        />
     </div>
   );
 };
